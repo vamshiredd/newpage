55d5b554acf24cefafe67a7959e90c6da82033ec
vamshiredd <46694446+vamshiredd@users.noreply.github.com>
2019-02-09 12:54:20 -0600

    added

---
+++ vamshi.js
@@ -0,0 +1,9 @@
+function multiplication(){
+
+        
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+    
+    let mul = parseInt(a)*parseInt(b);
+    alert("Multiplication of number 1 and number 2 gives "+mul);
+    }
