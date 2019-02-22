cb78fbdb51c487e6d2a71672d95d8193c8d1e805
vamshiredd <vamshikrishnay09@gmail.com>
2019-02-22 16:47:17 -0600

    Added Test Cases

--- vamshi.js
+++ vamshi.js
@@ -5,6 +5,10 @@ function multiplication(){
     let a = document.getElementById("num1").value;
     let b = document.getElementById("num2").value;
     
-    let mul = parseInt(a)*parseInt(b);
+    let mul = multiply(a,b);
     alert("Multiplication of number 1 and number 2 gives "+mul);
+}
+
+function multiply(x,y) {
+    return parseInt(x)*parseInt(y);
 }
