d7c752e44ee9c7b100e80e5a20eda39688b8e2ee
vamshiredd <vamshikrishnay09@gmail.com>
2019-03-03 18:19:27 -0600

    Added function.html and vamshi.js

--- vamshi.js
+++ vamshi.js
@@ -1,13 +1,13 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        var a = $('#num1').val();
+        var b = $('#num2').val();
+let mul = multiply(a,b);
+alert("Multiplication of number 1 and number 2 gives "+mul);
 
-function multiplication(){
+    });
+  });
 
-        
-    let a = document.getElementById("num1").value;
-    let b = document.getElementById("num2").value;
-    
-    let mul = multiply(a,b);
-    alert("Multiplication of number 1 and number 2 gives "+mul);
-}
 
 function multiply(x,y) {
     return parseInt(x)*parseInt(y);
